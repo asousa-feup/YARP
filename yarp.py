f4242b409db233d4821f69d6325fcfbbbf96f351
Armando Sousa <asousa@fe.up.pt>
2023-09-19 23:27:16 +0100

    cleanup + pencolor and size

--- yarp.py
+++ yarp.py
@@ -17,8 +17,7 @@ import time
 import math
 import numpy
 from sympy import Point2D, Line2D, Segment2D, intersection
-# from typing import TypeVar
-
+# from varname import nameof # https://pypi.org/project/varname/ # https://stackoverflow.com/questions/18425225/getting-the-name-of-a-variable-as-a-string
 
 # Initializations (global stuff) for MyDraw class
 hidden_turtle = turtle.Turtle()
@@ -28,6 +27,10 @@ hidden_turtle.speed(0)
 hidden_turtle.penup()
 
 
+
+#
+# ~~~~~~ Start of MyDraw class ~~~~~~
+#
 class MyDraw:
     """Algorithms for (robotics related) drawings (by using a hidden) turtle graphics """  
 
@@ -38,71 +41,24 @@ class MyDraw:
         hidden_turtle.penup()
         
 
-    def draw_seg(p1,p2, color="red"):
+    def draw_seg(p1,p2, color="orange", pensize : int=1):
+        prev_pensize = hidden_turtle.pensize()
+        prev_color = hidden_turtle.color()
+        hidden_turtle.pensize(pensize)
         hidden_turtle.penup()
         hidden_turtle.goto(p1[0],p1[1])
         hidden_turtle.pendown()
         hidden_turtle.color(color)
         hidden_turtle.goto(p2[0],p2[1])
         hidden_turtle.penup()
-
-    def draw_point(p, color : str ="red", pensize : int=1):
-        prev_pensize = hidden_turtle.pensize()
-        hidden_turtle.pensize(pensize)
-        MyDraw.draw_seg([p[0][0]-10, p[0][1]-10], [p[0][0]+10, p[0][1]+10], color)
-        MyDraw.draw_seg([p[0][0]-10, p[0][1]+10], [p[0][0]+10, p[0][1]-10], color)
         hidden_turtle.pensize(prev_pensize)
-
-    def perp( a ) :
-        b = numpy.empty_like(a)
-        b[0] = -a[1]
-        b[1] = a[0]
-        return b
-
-    # Deprecated hand coded intersection functions
-    #
-    # def line_intersect(p1,p2, p3,p4) :
-    #     """ Return the intersection of two (infinit) lines p1-p2 and p3-p4
-    #         Returns a false boolean if the lines don't intersect or the vector of the intersection point        
-    #     """
-    #     da = p2-p1
-    #     db = p4-p3
-    #     dp = p1-p3
-    #     dap = MyDraw.perp(da)
-    #     denom = numpy.dot( dap, db)
-    #     num = numpy.dot( dap, dp )
-    #     if (abs(denom) < 1e-5):
-    #         return False # parallel lines
-
-    #     return (num / denom.astype(float))*db + p3
-
-
-    # def seg_intersect(p1,p2, p3,p4) :
-    #     """ Return the intersection of two line segments p1-p2 and p3-p4
-    #         Returns a false boolean if the lines don't intersect or the vector of the intersection point        
-    #     """
-    #     ret = MyDraw.line_intersect(p1,p2, p3,p4)
+        hidden_turtle.color(prev_color[0], prev_color[1])
         
-    #     if ret is False: 
-    #         return False
-        
-    #     min_x=min(p1[0],p2[0])
-    #     max_x=max(p1[0],p2[0])
-    #     min_y=min(p1[1],p2[1])
-    #     max_y=max(p1[1],p2[1])
-    #     if ( (ret[0]<min_x) or (ret[0]>max_x) or 
-    #         (ret[1]<min_y) or (ret[1]>max_y) ):
-    #         return False # out of segment 1
 
-    #     min_x=min(p3[0],p4[0])
-    #     max_x=max(p3[0],p4[0])
-    #     min_y=min(p3[1],p4[1])
-    #     max_y=max(p3[1],p4[1])
-    #     if ( (ret[0]<min_x) or (ret[0]>max_x) or 
-    #         (ret[1]<min_y) or (ret[1]>max_y) ):
-    #         return False # out of segment 2
-        
-    #     return ret
+    def draw_point(p, color : str ="orange", pensize : int=1):
+        MyDraw.draw_seg([p[0][0]-10, p[0][1]-10], [p[0][0]+10, p[0][1]+10], color, pensize)
+        MyDraw.draw_seg([p[0][0]-10, p[0][1]+10], [p[0][0]+10, p[0][1]-10], color, pensize)
+
 
     def draw_quad(p1,p2,p3,p4, color="red"):
         MyDraw.draw_seg(p1,p2, color)
@@ -110,14 +66,11 @@ class MyDraw:
         MyDraw.draw_seg(p3,p4, color)
         MyDraw.draw_seg(p4,p1, color)
 
-
-
 #
-# Yarp class
+# ~~~~~~~~ Yarp class ~~~~~~~~
 #
 class Yarp(turtle.Turtle):
-    """Yet Another Robot in Python (YARP)
-    Drived from Turtle class
+    """Yet Another Robot in Python (YARP) -- Drived from Turtle class
     Add Glow Unglow methods and Selected property"""
 
     vel :   float = 0.0
@@ -161,10 +114,9 @@ class Yarp(turtle.Turtle):
         self.write(f"({self.pos()[0]:.2f} , {self.pos()[1]:.2f})".format(self.pos()[0],self.pos()[1]), align="center", font=("Arial Narrow", 10, "italic"))
         self.showturtle()
 
-    def put_at_mouse(x,y):
-        rob1.write(f"({x},{y})")
-        rob1.setpos(x,y)
-        turtle.ondrag(rob1.put_at_mouse, 1)
+    def place_robot_at(self, x,y):
+        print(f"(Will place robot at mouse coords {x},{y})")
+        self.setpos(x,y)
         
     def clear2paths():
         rob1.clear()
@@ -182,14 +134,15 @@ class Yarp(turtle.Turtle):
         rob1.ondrag(rob1.dragging)
 
     def glow(self, x, y):
-        print("Glow",self.__repr__(),f"x: {x} y: {y} ")
+        #print("Glow",self.__repr__(),f"x: {x} y: {y} ")
+        print("Glow",nameof(self),f"x: {x} y: {y} ")
         self.fillcolor("red")
         
     def unglow(self, x, y):
         print("unglow",self.__repr__(),f"x: {x} y: {y} ")
         self.fillcolor("")
 
-    def set_robot_pos(self, x : float, y : float , theta : float):
+    def set_robot_pose(self, x : float, y : float , theta : float):
         self.setpos(x ,y)
         self.setheading(theta)
 
@@ -205,8 +158,9 @@ class Yarp(turtle.Turtle):
             print("Out of Bounds!", rob1.pos())
             print("Scr Size", turtle.Screen().screensize())
             Yarp.byebye()
+
         # check if the robots collided           
-        if math.dist(rob1.pos(),rob2.pos())<50:
+        if math.dist(rob1.pos(),rob2.pos())<40:
             print("Robots collided!")
             Yarp.byebye()
 
@@ -225,7 +179,9 @@ class Yarp(turtle.Turtle):
         rob2.vel+=2
         rob1.omega+=random.randrange(-10,10)
         rob2.omega+=random.randrange(-5,int(rob2.vel//4))
-
+        
+        # rob1.set_robot_pose(rob1.pos()[0], rob1.pos()[1], rob1.heading())
+        
         if (int(Yarp.yarp_time*2) % 5 == 0):
             print ("Rob1 pose: ", rob1.pos(), rob1.heading(), "R1_vels=", rob1.vel, rob1.omega)
             print ("Rob2 pose: ", rob2.pos(), rob2.heading(), "R2_vels=", rob2.vel, rob2.omega)
@@ -235,8 +191,7 @@ class Yarp(turtle.Turtle):
    
 # Start of main code
 
-
-turtle.Screen().setup(800,600)
+#turtle.Screen().setup(800,600)
 turtle.Screen().screensize( canvwidth=800, canvheight=600 ) # not working ??
 time.sleep(0.1)
 print("Scr Size", turtle.Screen().screensize())
@@ -257,6 +212,8 @@ rob2.goto(100,100)
 rob2.pendown()
 
 if False: # Test code for turtle healthiness of coordinates
+    def exit_program():
+        exit()
     time.sleep(0.1)
     rob1.setpos(0,0)
     rob1.setheading(0)
@@ -276,8 +233,13 @@ if False: # Test code for turtle healthiness of coordinates
     rob2.forward(math.sqrt(2)*200)
     rob2.back(math.sqrt(2)*400)
     rob2.write_pos()
+    rob2.write("Press ESC again to exit", align="center", font = ("Courier", 24, "normal"))
     time.sleep(2)
+    turtle.listen()
+    turtle.onkeypress(exit_program, "Escape")
+    turtle.mainloop()
     exit()
+    
 
 # Bind the arrow keys and other keys to the functions
 turtle.listen()
@@ -286,7 +248,7 @@ turtle.listen()
 # ~~~~~~ Start of Test line draw and intersection ~~~~~~
 #
 
-if False:            # Test line draw and intersection
+if False:            # Test line draw and intersection "Unit Test"
     random.seed(10)
 
     for i in range(6):
@@ -357,10 +319,9 @@ while True:
     #turtle.ondrag(turtle.goto,1, True)
     turtle.onscreenclick(rob1.right_click, 3, True)
     turtle.onscreenclick(rob2.right_click, 2, True)
+    turtle.onscreenclick(rob1.place_robot_at, 1, True)
 
-    time.sleep(1)
-
-    # Keep window open
+    # Transfer control to Tkinter process events
     turtle.mainloop()
     
         
